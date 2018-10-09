58e0d6d2b4d68457cfe813a6bd45eabc5b25a8c8
Luis99B <30879359+Luis99B@users.noreply.github.com>
2018-10-09 08:31:00 -0500

    Update materialize, add Google Analytics & own js

--- js/propio.js
+++ js/propio.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+});
